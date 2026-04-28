3bb8966e19ab5fe923708e6aae54e8aa431c2ea6
Salil Koirala <38308450+salilkoirala46@users.noreply.github.com>
2026-04-28 05:41:33 +0000

    Remove vendor ziggy alias so Docker node build stage works

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,6 @@ export default defineConfig({
     resolve: {
         alias: {
             '@': resolve(__dirname, 'resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
         },
     },
     server: {
